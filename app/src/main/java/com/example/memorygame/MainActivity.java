f26a92ca22008905697de710da34094503380851
Cai Yaolong Derek <44289562+caiylderek@users.noreply.github.com>
2021-01-06 15:21:35 +0800

    Changed from AsyncTask to Thread + Handler

--- app/src/main/java/com/example/memorygame/MainActivity.java
+++ app/src/main/java/com/example/memorygame/MainActivity.java
@@ -8,10 +8,13 @@ import android.content.AsyncQueryHandler;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.webkit.URLUtil;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -25,13 +28,17 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, AsyncResponse {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     RecyclerView recyclerView;
     RecyclerAdapter recyclerAdapter;
     ProgressBar progressBar;
     TextView textView;
+    Thread imageProcess = null;
+    boolean imageReady = false;
+    Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +55,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textView = findViewById(R.id.textView);
 
         findViewById(R.id.fetchButton).setOnClickListener(this);
+
     }
 
     @Override
@@ -56,7 +64,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             // TODO: Scrape website and populate GridView
             TextInputEditText input = findViewById(R.id.textInputEditText);
             if (input.getText() != null) {
-                new LoadImagesLinksTask(this).execute(input.getText().toString());
+                if (imageProcess != null) {
+                    imageProcess.interrupt();
+                }
+                imageProcess = new Thread(new LoadImagesThread(input.getText().toString()));
+                imageProcess.start();
+                // new LoadImagesLinksTask(this).execute(input.getText().toString());
             }
 
             // Starts game activity
@@ -65,21 +78,104 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    @Override
-    public void processFinish(boolean output) {
-        if (output) {
-            recyclerAdapter.notifyDataSetChanged();
+    private class LoadImagesThread implements Runnable {
+        private String url;
+        private final ArrayList<String> imageLinks = new ArrayList<>();
+        Elements images;
+
+        LoadImagesThread(String url) {
+            this.url = url;
+        }
+        @Override
+        public void run() {
+            if (!URLUtil.isValidUrl(url)) {
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "Invalid URL, please try again.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                return;
+            }
+            startUI();
+            if (url.endsWith("/")) {
+                url = url.substring(0, url.length() - 1);
+            }
+            try {
+                Document doc = Jsoup.connect(url).get();
+                images = doc.select("img[src~=(?i).(gif|png|jpe?g)]");
+                progressBar.setMax(images.size());
+                for (int i = 0; i < images.size(); i++) {
+                    Thread.sleep(150);
+                    if (Thread.interrupted()) {
+                        closeThread(false);
+                    }
+                    Element e = images.get(i);
+                    updateUI(i + 1);
+                    String sourceAttribute = e.attr("src");
+                    if (!sourceAttribute.startsWith("http")) {
+                        imageLinks.add(url + sourceAttribute);
+                    } else {
+                        imageLinks.add(sourceAttribute);
+                    }
+                    Log.e("TESTING", sourceAttribute);
+                    recyclerAdapter.setUrls(imageLinks);
+                }
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+                closeThread(false);
+            }
+            closeThread(true);
+        }
+
+        private void startUI() {
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    recyclerAdapter.setUrls(imageLinks);
+                    recyclerAdapter.notifyDataSetChanged();
+                    progressBar.setProgress(0);
+                    progressBar.setVisibility(View.VISIBLE);
+                    textView.setText("Starting to process images..");
+                    textView.setVisibility(View.VISIBLE);
+                }
+            });
+        }
+
+        private void updateUI(int progress) {
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    progressBar.setProgress(progress);
+                    textView.setText(String.format(Locale.ENGLISH, "Processing img element %d of %d...", progress, images.size()));
+                    recyclerAdapter.notifyItemChanged(progress - 1);
+                }
+            });
+
+        }
+
+        private void closeThread(boolean success) {
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    if (success) {
+                        Toast.makeText(getApplicationContext(), "Images processed!", Toast.LENGTH_SHORT).show();
+                        imageReady = true;
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Image loading failed!", Toast.LENGTH_SHORT).show();
+                        imageReady = false;
+                    }
+                    progressBar.setVisibility(View.INVISIBLE);
+                    textView.setVisibility(View.INVISIBLE);
+                    imageProcess = null;
+                }
+            });
         }
     }
 
     public class LoadImagesLinksTask extends AsyncTask<String, Integer, Void> {
-        public AsyncResponse delegate;
         private final ArrayList<String> imageLinks = new ArrayList<>();
         private int size;
-
-        public LoadImagesLinksTask(AsyncResponse delegate) {
-            this.delegate = delegate;
-        }
 
         @Override
         protected Void doInBackground(String... urls) {
@@ -90,7 +186,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 try {
                     Document doc = Jsoup.connect(url).get();
-                    Elements images = doc.getElementsByTag("img");
+                    Elements images = doc.select("img[src~=(?i).(gif|png|jpe?g)]");
                     size = images.size();
                     progressBar.setMax(images.size());
                     for (int i = 0; i < images.size(); i++) {
@@ -102,15 +198,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         } else {
                             imageLinks.add(sourceAttribute);
                         }
+                        Log.e("TESTING", sourceAttribute);
+                        recyclerAdapter.setUrls(imageLinks);
+                        updateUI(i);
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
+                    recyclerAdapter.setUrls(imageLinks);
                 }
             }
 
             progressBar.setMax(imageLinks.size());
 
-            recyclerAdapter.setUrls(imageLinks);
+//            recyclerAdapter.setUrls(imageLinks);
 
             return null;
         }
@@ -118,6 +218,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
+            recyclerAdapter.setUrls(imageLinks);
             textView.setText("");
             progressBar.setProgress(0);
         }
@@ -132,8 +233,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             for (String s : imageLinks) {
                 Log.d("test", s);
             }
-            textView.setText("Processing URL elements done!");
-            delegate.processFinish(imageLinks.size() > 0);
+            textView.setText("Done!");
+        }
+
+        void updateUI(int position) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    recyclerAdapter.notifyItemChanged(position);
+                }
+            });
         }
     }
 }
